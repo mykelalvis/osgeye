0d3ae730b1b1744f8fbdc0c1f718a1812a1ef5c2
corey.baswell <corey.baswell@277a9e92-e1fc-11de-b950-c1e2c078a2c5>
2010-02-08 13:12:38 +0000

    Removed type definition from Service and ServiceClass.

--- trunk/base/src/org/osgeye/domain/ServiceClass.java
+++ trunk/base/src/org/osgeye/domain/ServiceClass.java
@@ -2,27 +2,27 @@ package org.osgeye.domain;
 
 import java.io.Serializable;
 
-import org.osgeye.remotereflect.TypeDefinition;
-
 public class ServiceClass implements Serializable, Comparable<ServiceClass>
 {
+  private final String className;
+  
   private Service service;
   
-  private TypeDefinition typeDefinition;
-  
-  private String className;
+  private final boolean interfce;
 
   public ServiceClass(String className, Service service)
   {
     this.className = className;
     this.service = service;
+    interfce = false;
   }
 
-  public ServiceClass(TypeDefinition typeDefinition, Service service)
+  
+  public ServiceClass(Class clazz, Service service)
   {
+    this.className = clazz.getCanonicalName();
     this.service = service;
-    this.typeDefinition = typeDefinition;
-    className = typeDefinition.getQualifiedName();
+    this.interfce = clazz.isInterface();
   }
   
   public String getClassName()
@@ -37,7 +37,7 @@ public class ServiceClass implements Serializable, Comparable<ServiceClass>
 
   public boolean isInterface()
   {
-    return (typeDefinition == null) ?  false : typeDefinition.isInterface();
+    return interfce;
   }
   
   public String toString()
@@ -57,6 +57,7 @@ public class ServiceClass implements Serializable, Comparable<ServiceClass>
       return false;
     }
   }
+
 
   public int compareTo(ServiceClass sc)
   {
